22823965a17a00133348e3681beca29837a80e12
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-15 18:00:38 -0300

    feat(domain): add repository interfaces for EmpresaOrganizacao aggregates

---
+++ WorkWell.Domain/Interfaces/EmpresaOrganizacao/IEmpresaRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorkWell.Domain.Entities.EmpresaOrganizacao;
+
+namespace WorkWell.Domain.Interfaces.EmpresaOrganizacao
+{
+    public interface IEmpresaRepository
+    {
+        Task<Empresa?> GetByIdAsync(long id);
+        Task<IEnumerable<Empresa>> GetAllAsync();
+        Task AddAsync(Empresa empresa);
+        Task UpdateAsync(Empresa empresa);
+        Task DeleteAsync(long id);
+    }
+}
---
+++ WorkWell.Domain/Interfaces/EmpresaOrganizacao/IFuncionarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorkWell.Domain.Entities.EmpresaOrganizacao;
+
+namespace WorkWell.Domain.Interfaces.EmpresaOrganizacao
+{
+    public interface IFuncionarioRepository
+    {
+        Task<Funcionario?> GetByIdAsync(long id);
+        Task<IEnumerable<Funcionario>> GetAllAsync();
+        Task AddAsync(Funcionario funcionario);
+        Task UpdateAsync(Funcionario funcionario);
+        Task DeleteAsync(long id);
+    }
+}
