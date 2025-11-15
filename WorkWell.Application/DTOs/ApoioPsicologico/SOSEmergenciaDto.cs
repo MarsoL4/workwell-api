15229c94c0b3442bbc077bc2073840ed6c26ba6e
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-15 19:33:34 -0300

    feat(SOSEmergencia): add SOSEmergenciaDto, repository interface and implementation

---
+++ WorkWell.Application/DTOs/ApoioPsicologico/SOSEmergenciaDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WorkWell.Application.DTOs.ApoioPsicologico
+{
+    public class SOSemergenciaDto
+    {
+        public long Id { get; set; }
+        public long FuncionarioId { get; set; }
+        public DateTime DataAcionamento { get; set; }
+        public string Tipo { get; set; } = null!;
+        public bool PsicologoNotificado { get; set; }
+    }
+}
