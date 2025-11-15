dda411c7e2b4134b24dc4cf66d850ae7cbd75c3d
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-15 18:01:14 -0300

    feat(infrastructure): implement DbContext, repository implementations and DI setup

---
+++ WorkWell.Infrastructure/Configurations/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WorkWell.Infrastructure.Persistence;
+
+namespace WorkWell.Infrastructure.Configurations
+{
+    public static class InfrastructureDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Usa Oracle. O pacote Oracle.EntityFrameworkCore deve estar instalado.
+            services.AddDbContext<WorkWellDbContext>(options =>
+                options.UseOracle(configuration.GetConnectionString("OracleConnection")));
+
+            return services;
+        }
+    }
+}
---
+++ WorkWell.Infrastructure/Repositories/EmpresaOrganizacao/EmpresaRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WorkWell.Domain.Entities.EmpresaOrganizacao;
+using WorkWell.Domain.Interfaces.EmpresaOrganizacao;
+using WorkWell.Infrastructure.Persistence;
+
+namespace WorkWell.Infrastructure.Repositories.EmpresaOrganizacao
+{
+    public class EmpresaRepository : IEmpresaRepository
+    {
+        private readonly WorkWellDbContext _context;
+
+        public EmpresaRepository(WorkWellDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Empresa?> GetByIdAsync(long id)
+        {
+            return await _context.Empresas.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Empresa>> GetAllAsync()
+        {
+            return await _context.Empresas.ToListAsync();
+        }
+
+        public async Task AddAsync(Empresa empresa)
+        {
+            await _context.Empresas.AddAsync(empresa);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Empresa empresa)
+        {
+            _context.Empresas.Update(empresa);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(long id)
+        {
+            var empresa = await _context.Empresas.FindAsync(id);
+            if (empresa != null)
+            {
+                _context.Empresas.Remove(empresa);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
