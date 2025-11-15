c267a2d95528f2fcfb3c55bdf1ddd36713617254
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-15 18:08:01 -0300

    feat(infrastructure): implement WorkWellDbContext with DbSet mappings

--- WorkWell.Infrastructure/Persistence/WorkWellDbContext.cs
+++ WorkWell.Infrastructure/Persistence/WorkWellDbContext.cs
@@ -1,12 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using WorkWell.Domain.Entities.EmpresaOrganizacao;
+using WorkWell.Domain.Entities.ApoioPsicologico;
+using WorkWell.Domain.Entities.AtividadesBemEstar;
+using WorkWell.Domain.Entities.Enquetes;
+using WorkWell.Domain.Entities.Indicadores;
+using WorkWell.Domain.Entities.AvaliacoesEmocionais;
+using WorkWell.Domain.Entities.Notificacoes;
+using WorkWell.Domain.Entities.OmbudMind;
+using WorkWell.Domain.Entities.Agenda;
 
 namespace WorkWell.Infrastructure.Persistence
 {
-    internal class WorkWellDbContext
+    public class WorkWellDbContext : DbContext
     {
+        public WorkWellDbContext(DbContextOptions<WorkWellDbContext> options) : base(options) { }
+
+        // Empresa & organização
+        public DbSet<Empresa> Empresas => Set<Empresa>();
+        public DbSet<Funcionario> Funcionarios => Set<Funcionario>();
+        public DbSet<Setor> Setores => Set<Setor>();
+
+        // Apoio Psicológico
+        public DbSet<Psicologo> Psicologos => Set<Psicologo>();
+        public DbSet<ConsultaPsicologica> ConsultasPsicologicas => Set<ConsultaPsicologica>();
+        public DbSet<ChatAnonimo> ChatsAnonimos => Set<ChatAnonimo>();
+        public DbSet<SOSemergencia> SOSemergencias => Set<SOSemergencia>();
+
+        // Atividades de Bem-Estar
+        public DbSet<AtividadeBemEstar> AtividadesBemEstar => Set<AtividadeBemEstar>();
+        public DbSet<ParticipacaoAtividade> ParticipacoesAtividade => Set<ParticipacaoAtividade>();
+
+        // Enquetes
+        public DbSet<Enquete> Enquetes => Set<Enquete>();
+        public DbSet<RespostaEnquete> RespostasEnquete => Set<RespostaEnquete>();
+
+        // Indicadores
+        public DbSet<IndicadoresEmpresa> IndicadoresEmpresa => Set<IndicadoresEmpresa>();
+
+        // Avaliações emocionais
+        public DbSet<AvaliacaoProfunda> AvaliacoesProfundas => Set<AvaliacaoProfunda>();
+        public DbSet<MoodCheck> MoodChecks => Set<MoodCheck>();
+        public DbSet<RiscoPsicossocial> RiscosPsicossociais => Set<RiscoPsicossocial>();
+        public DbSet<PerfilEmocional> PerfisEmocionais => Set<PerfilEmocional>();
+
+        // Notificações
+        public DbSet<Notificacao> Notificacoes => Set<Notificacao>();
+
+        // OmbudMind
+        public DbSet<Denuncia> Denuncias => Set<Denuncia>();
+        public DbSet<InvestigacaoDenuncia> InvestigacoesDenuncia => Set<InvestigacaoDenuncia>();
+
+        // Agenda
+        public DbSet<AgendaFuncionario> AgendasFuncionarios => Set<AgendaFuncionario>();
+        public DbSet<ItemAgenda> ItensAgenda => Set<ItemAgenda>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Espaço para configurações Fluent API adicionais no futuro
+        }
     }
 }
