2f04528ac6bb963e365eea5de568bf9c958d1448
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-15 19:26:51 -0300

    feat(apoio-psicologico): add PsicologoDto, repository interface and implementation

---
+++ WorkWell.Domain/Interfaces/ApoioPsicologico/IPsicologoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorkWell.Domain.Entities.ApoioPsicologico;
+
+namespace WorkWell.Domain.Interfaces.ApoioPsicologico
+{
+    public interface IPsicologoRepository
+    {
+        Task<Psicologo?> GetByIdAsync(long id);
+        Task<IEnumerable<Psicologo>> GetAllAsync();
+        Task AddAsync(Psicologo psicologo);
+        Task UpdateAsync(Psicologo psicologo);
+        Task DeleteAsync(long id);
+    }
+}
---
+++ WorkWell.Infrastructure/Repositories/ApoioPsicologico/PsicologoRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WorkWell.Domain.Entities.ApoioPsicologico;
+using WorkWell.Domain.Interfaces.ApoioPsicologico;
+using WorkWell.Infrastructure.Persistence;
+
+namespace WorkWell.Infrastructure.Repositories.ApoioPsicologico
+{
+    public class PsicologoRepository : IPsicologoRepository
+    {
+        private readonly WorkWellDbContext _context;
+
+        public PsicologoRepository(WorkWellDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Psicologo?> GetByIdAsync(long id)
+        {
+            return await _context.Psicologos.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Psicologo>> GetAllAsync()
+        {
+            return await _context.Psicologos.ToListAsync();
+        }
+
+        public async Task AddAsync(Psicologo psicologo)
+        {
+            await _context.Psicologos.AddAsync(psicologo);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Psicologo psicologo)
+        {
+            _context.Psicologos.Update(psicologo);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(long id)
+        {
+            var psicologo = await _context.Psicologos.FindAsync(id);
+            if (psicologo != null)
+            {
+                _context.Psicologos.Remove(psicologo);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
