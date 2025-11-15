a3352ed273d1478689323b95ca660826241837eb
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-15 20:06:01 -0300

    feat(avaliacoes-emocionais): add DTOs, repository interfaces and implementations for emotional assessments

---
+++ WorkWell.Application/DTOs/AvaliacoesEmocionais/RiscoPsicossocialDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WorkWell.Application.DTOs.AvaliacoesEmocionais
+{
+    public class RiscoPsicossocialDto
+    {
+        public long Id { get; set; }
+        public long FuncionarioId { get; set; }
+        public string Categoria { get; set; } = null!;
+        public int NivelRisco { get; set; }
+        public DateTime DataRegistro { get; set; }
+    }
+}
