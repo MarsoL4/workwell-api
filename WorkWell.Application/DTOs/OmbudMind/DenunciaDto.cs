7e35ae615d4664d79bd2f6c35093300ac5cd1ee2
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-16 19:06:28 -0300

    feat(ombudmind): add DTOs and repository interfaces/implementations for Denuncia and InvestigacaoDenuncia

---
+++ WorkWell.Application/DTOs/OmbudMind/DenunciaDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WorkWell.Domain.Enums.OmbudMind;
+
+namespace WorkWell.Application.DTOs.OmbudMind
+{
+    public class DenunciaDto
+    {
+        public long Id { get; set; }
+        public long? FuncionarioDenuncianteId { get; set; }
+        public long EmpresaId { get; set; }
+        public TipoDenuncia Tipo { get; set; }
+        public string Descricao { get; set; } = null!;
+        public DateTime DataCriacao { get; set; }
+        public StatusDenuncia Status { get; set; }
+        public string CodigoRastreamento { get; set; } = null!;
+    }
+}
