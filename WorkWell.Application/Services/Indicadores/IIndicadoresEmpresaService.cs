13498993405508c7b9ba0ab04e82dbb1174f38a9
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-16 19:36:01 -0300

    feat(service): add Application Services for IndicadoresEmpresa

---
+++ WorkWell.Application/Services/Indicadores/IIndicadoresEmpresaService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorkWell.Application.DTOs.Indicadores;
+
+namespace WorkWell.Application.Services.Indicadores
+{
+    public interface IIndicadoresEmpresaService
+    {
+        Task<IEnumerable<IndicadoresEmpresaDto>> GetAllAsync();
+        Task<IndicadoresEmpresaDto?> GetByIdAsync(long id);
+        Task<long> CreateAsync(IndicadoresEmpresaDto dto);
+        Task UpdateAsync(IndicadoresEmpresaDto dto);
+        Task DeleteAsync(long id);
+    }
+}
