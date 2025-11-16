5c8db85aa1786df42860ef61ac59fb78ea08b286
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-16 18:59:59 -0300

    feat(notificacao): add DTO and repository infrastructure for Notificacao

---
+++ WorkWell.Domain/Interfaces/Notificacoes/INotificacaoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorkWell.Domain.Entities.Notificacoes;
+
+namespace WorkWell.Domain.Interfaces.Notificacoes
+{
+    public interface INotificacaoRepository
+    {
+        Task<Notificacao?> GetByIdAsync(long id);
+        Task<IEnumerable<Notificacao>> GetAllAsync();
+        Task<IEnumerable<Notificacao>> GetAllByFuncionarioIdAsync(long funcionarioId);
+        Task AddAsync(Notificacao notificacao);
+        Task UpdateAsync(Notificacao notificacao);
+        Task DeleteAsync(long id);
+    }
+}
