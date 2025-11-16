54e9231e28b4d7a8743e0574cde35f4476807cfa
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-16 19:38:28 -0300

    feat(service): add Application Services for Notificacao

---
+++ WorkWell.Application/Services/Notificacoes/INotificacaoService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorkWell.Application.DTOs.Notificacoes;
+
+namespace WorkWell.Application.Services.Notificacoes
+{
+    public interface INotificacaoService
+    {
+        Task<IEnumerable<NotificacaoDto>> GetAllAsync();
+        Task<NotificacaoDto?> GetByIdAsync(long id);
+        Task<IEnumerable<NotificacaoDto>> GetAllByFuncionarioIdAsync(long funcionarioId);
+        Task<long> CreateAsync(NotificacaoDto dto);
+        Task UpdateAsync(NotificacaoDto dto);
+        Task DeleteAsync(long id);
+    }
+}
