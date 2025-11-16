289f46094e32253d92b17d1938df0637e00dfaee
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-16 19:14:56 -0300

    feat(service): add and implement Setor Service class and interface

---
+++ WorkWell.Application/Services/EmpresaOrganizacao/ISetorService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorkWell.Application.DTOs.EmpresaOrganizacao;
+
+namespace WorkWell.Application.Services.EmpresaOrganizacao
+{
+    public interface ISetorService
+    {
+        Task<IEnumerable<SetorDto>> GetAllAsync();
+        Task<SetorDto?> GetByIdAsync(long id);
+        Task<long> CreateAsync(SetorDto setorDto);
+        Task UpdateAsync(SetorDto setorDto);
+        Task DeleteAsync(long id);
+    }
+}
