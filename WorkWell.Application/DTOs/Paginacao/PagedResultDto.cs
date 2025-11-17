eeddd683139153be42519d8d7c727c3f028f4405
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-17 19:33:45 -0300

    feat: adicionar DTOs de paginação e extension de IQueryable para suporte à paginação

---
+++ WorkWell.Application/DTOs/Paginacao/PagedResultDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WorkWell.Application.DTOs.Paginacao
+{
+    public class PagedResultDto<T>
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public IEnumerable<T> Items { get; set; } = Enumerable.Empty<T>();
+    }
+}
