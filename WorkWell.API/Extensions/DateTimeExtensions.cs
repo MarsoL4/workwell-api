c82475a86bbbf63d36670558a960a69cfab3b53c
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-19 19:32:47 -0300

    refactor: remove extensão DateTimeExtensions da camada API (será movida para Domain compartilhado)

--- WorkWell.API/Extensions/DateTimeExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WorkWell.Domain.Extensions
-{
-    public static class DateTimeExtensions
-    {
-        public static DateTime TruncateToMinutes(this DateTime dt)
-            => new(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0, dt.Kind);
-    }
-}
